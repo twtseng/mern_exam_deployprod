df5e33a87c5b4ac51ab2df962211a1eeb8cba8b8
twtseng <twtseng@users.noreply.github.com>
2020-09-18 12:14:24 -0700

    build

---
+++ client/build/precache-manifest.609cfa472dc940a62e2ed5aaec6d97bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6591d4c338a7d16a945590826a2c8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9740c09446bdceb8831",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "832118ea1ffc234fc6c5",
+    "url": "/static/css/main.3b515cc3.chunk.css"
+  },
+  {
+    "revision": "d9740c09446bdceb8831",
+    "url": "/static/js/2.58707b2a.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.58707b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832118ea1ffc234fc6c5",
+    "url": "/static/js/main.1c005703.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
